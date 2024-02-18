c127f1155258e53298e6acf2c0cc56f457f8b302
milushh <154788726+milushh@users.noreply.github.com>
2024-02-18 20:49:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+function addDiv(){
+    let div = document.getElementById('openClose')
+    let btn = document.getElementById('btn')
+    // div.classList.add("open")
+    btn.innerText = btn.innerText == "-" ? "+" : "-"
+    div.classList =div.classList == "close"? "open":"close"
+    
+    
+
+}
+
+function addDiv2(){
+    let div = document.getElementById('openCloses')
+    let btn2 = document.getElementById('btn2')
+    let btn3 = document.getElementById('btn3')
+    // div.classList.add("open")
+    btn2.innerText = btn3.innerText == "-" ? "+" : "-"
+  
+    div.classList =div.classList == "close"? "open":"close"
+  
+   
+  
+}
+
+
+function addDiv3(){
+    let div = document.getElementById('openClosess')
+    let btn1 = document.getElementById('btn88')
+    let btn2 = document.getElementById('btn23')
+    // div.classList.add("open")
+    div.classList = div.classList == "close"? "open":"close"
+    btn1.innerText = btn2.innerText == "-" ? "+" : "-"
+
+    
+    
+
+}
+
+function addDiv4(){
+    let div = document.getElementById('openClosess')
+    let btn2 = document.getElementById('btn23')
+    let btn3 = document.getElementById('btn25')
+   
+    btn2.innerText = btn2.innerText == "-" ? "+" : "-"
+    div.classList =div.classList == "close"? "open":"close"
+
+   
+  
+}
+
+
+
+
+   
